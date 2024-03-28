10ec6bbf37aa5ca402a15fff372673952783f2de
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-03-28 22:43:00 +0900

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Activate virtual environment
+. /venv/bin/activate
+
+# Start JupyterLab
+pipenv run jupyter lab --notebook-dir=/notebooks --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+
+# Start Nginx
 envsubst < /app/nginx.conf > /etc/nginx/nginx.conf
 nginx
