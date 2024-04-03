a29c92e53424ef881fcc8b51c990aeed204e26e2
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-04-03 22:58:34 +0900

    Create generate_certs.sh

---
+++ docker/generate_certs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set working directory within the script
+cd /etc/nginx/certs
+
+# Certificate Generation Logic
+openssl genrsa -out _wildcard.quantumworkspace.dev+3-key.pem 2048 && \
+openssl req -new -key _wildcard.quantumworkspace.dev+3-key.pem -out _wildcard.quantumworkspace.dev+3.csr -subj "/CN=*.quantumworkspace.dev" -config <( \
+cat <<-EOF \
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+distinguished_name = dn
+
+[dn]
+CN = *.quantumworkspace.dev 
+
+[SAN]
+subjectAltName = DNS:*.quantumworkspace.dev, DNS:localhost, IP:127.0.0.1, IP:::1
+EOF
+) && \
+openssl x509 -req -days 365 -in _wildcard.quantumworkspace.dev+3.csr -signkey _wildcard.quantumworkspace.dev+3-key.pem -out _wildcard.quantumworkspace.dev+3.pem && \
+openssl dhparam -out dhparam.pem 2048
