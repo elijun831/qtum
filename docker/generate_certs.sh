95ee5e30093e139bbd0366ad6ace3873d1e4c730
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-04-08 23:16:25 +0900

    Update generate_certs.sh

--- docker/generate_certs.sh
+++ docker/generate_certs.sh
@@ -9,7 +9,9 @@ openssl req -x509 -new -nodes -key Root_CA.key -sha256 -days 365 -out Root_CA.pe
 
 # --- Server certificate generation ---
 openssl genrsa -out _wildcard.quantumworkspace.dev+3-key.pem 4096
-openssl req -new -key _wildcard.quantumworkspace.dev+3-key.pem -out _wildcard.quantumworkspace.dev+3.csr -subj "/CN=*.quantumworkspace.dev" -config <( \
+
+# Create a self-signed certificate directly from the private key
+openssl req -x509 -new -nodes -key _wildcard.quantumworkspace.dev+3-key.pem -sha256 -days 365 -out _wildcard.quantumworkspace.dev+3.pem -subj "/CN=*.quantumworkspace.dev" -config <( \
 cat <<-EOF \
 [req]
 default_bits = 4096
@@ -24,10 +26,9 @@ CN = *.quantumworkspace.dev 
 subjectAltName = DNS:*.quantumworkspace.dev, DNS:localhost, IP:127.0.0.1, IP:::1
 EOF
 )
-openssl x509 -req -days 365 -in _wildcard.quantumworkspace.dev+3.csr -CA Root_CA.pem -CAkey Root_CA.key -CAcreateserial -out _wildcard.quantumworkspace.dev+3.pem -sha256
 
 # --- DH parameters generation ---
 openssl dhparam -out dhparam.pem 4096
 
 # --- Root CA certificate conversion (PEM -> DER) ---
-openssl x509 -in Root_CA.pem -out Root_CA.crt -outform DER 
+openssl x509 -in Root_CA.pem -out Root_CA.crt -outform DER 
