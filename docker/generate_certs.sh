21ec17296e2d0da8f5b2943ea31f014355abbb28
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-04-08 22:14:46 +0900

    Update generate_certs.sh

--- docker/generate_certs.sh
+++ docker/generate_certs.sh
@@ -3,8 +3,12 @@
 # Set working directory within the script
 cd /etc/nginx/certs
 
-# Certificate Generation Logic
-openssl genrsa -out _wildcard.quantumworkspace.dev+3-key.pem 4096 && \
+# --- Root CA certificate generation ---
+openssl genrsa -out Root_CA.key 2048
+openssl req -x509 -new -nodes -key Root_CA.key -sha256 -days 1024 -out Root_CA.pem -subj "/C=US/ST=Boston/L=Gangnam/O=My Organization/OU=IT Department/CN=My Root CA"
+
+# --- Server certificate generation ---
+openssl genrsa -out _wildcard.quantumworkspace.dev+3-key.pem 4096
 openssl req -new -key _wildcard.quantumworkspace.dev+3-key.pem -out _wildcard.quantumworkspace.dev+3.csr -subj "/CN=*.quantumworkspace.dev" -config <( \
 cat <<-EOF \
 [req]
@@ -14,11 +18,16 @@ default_md = sha256
 distinguished_name = dn
 
 [dn]
-CN = *.quantumworkspace.dev 
+CN = *.quantumworkspace.dev 
 
 [SAN]
 subjectAltName = DNS:*.quantumworkspace.dev, DNS:localhost, IP:127.0.0.1, IP:::1
 EOF
-) && \
-openssl x509 -req -days 365 -in _wildcard.quantumworkspace.dev+3.csr -signkey _wildcard.quantumworkspace.dev+3-key.pem -out _wildcard.quantumworkspace.dev+3.pem && \
+)
+openssl x509 -req -days 365 -in _wildcard.quantumworkspace.dev+3.csr -CA Root_CA.pem -CAkey Root_CA.key -CAcreateserial -out _wildcard.quantumworkspace.dev+3.pem -sha256
+
+# --- DH parameters generation ---
 openssl dhparam -out dhparam.pem 4096
+
+# --- Root CA certificate conversion (PEM -> DER) ---
+openssl x509 -in Root_CA.pem -out Root_CA.crt -outform DER 
